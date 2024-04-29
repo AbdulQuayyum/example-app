99ccbe2dee9f06106b5e45a45c3cc8564f54355d
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2024-04-29 17:06:58 +0100

    Worked on post function...

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Product;
+
 class HomeController extends Controller
 {
-    public function index() {
-        return view('test');
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function AddProduct(Request $request)
+    {
+        $data = new Product;
+        $data->title = $request->productTitle;
+        $data->description = $request->productDesc;
+        if ($request->hasFile('productImg')) {
+            $image = $request->productImg;
+            $imagename = time() . '.' . $image->getClientOriginalExtension();
+            $image->move('product', $imagename);
+            $data->image = $imagename;
+        } else {
+            return redirect()->back()->with('error', 'Please upload an image.');
+        }
+        $data->save();
+
+        return redirect()->back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/Home', function () {
-    return view('home');
-});
-
-route::get('test', [HomeController::class, 'index']);
+route::post('/AddProduct', [HomeController::class, 'AddProduct']);
